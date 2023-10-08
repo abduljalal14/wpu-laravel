79066bc968725d17407d9dfe7b97e410685aa8b7
abduljalal14 <abdoeljalalexotic@gmail.com>
2023-10-09 06:06:04 +0700

    Post Category

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,13 +3,15 @@
 
 @section('container')
 
-<article class="mb-5">
+<h1 class="mb-5">Post Category : {{ $category }}</h1>
+
     @foreach ($posts as $post)
-        <h1>
+    <article>
+        <h2>
             <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-        </h1>
+        </h2>
         <p>{{ $post->excerpt }}</p>
+    </article>      
     @endforeach
-</article>
     
 @endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,9 @@
 
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
+    
+    <P>By. Abdul Jalal in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</P></a>
+
     {!! $post->body !!}
 
     <a href="/posts">Back to Posts</a>
